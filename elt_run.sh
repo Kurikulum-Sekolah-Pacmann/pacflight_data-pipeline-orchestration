a4b11ca1c15358c1b9e4d46311faa8ea99ae1128
rahilaode <laoderahilatansa@gmail.com>
2024-12-16 18:07:37 +0700

    update

--- elt_run.sh
+++ elt_run.sh
@@ -3,13 +3,13 @@
 echo "========== Start Orcestration Process =========="
 
 # Virtual Environment Path
-VENV_PATH="/home/laode/pacmann/project/pacflight_data-pipeline-orchestration/.venv/bin/activate"
+VENV_PATH="/home/laode/pacmann/course/data-storage/week-5/pacflight_data-pipeline-orchestration/.venv/bin/activate"
 
 # Activate Virtual Environment
 source "$VENV_PATH"
 
 # Set Python script
-PYTHON_SCRIPT="/home/laode/pacmann/project/pacflight_data-pipeline-orchestration/elt_main.py"
+PYTHON_SCRIPT="/home/laode/pacmann/course/data-storage/week-5/pacflight_data-pipeline-orchestration/elt_main.py"
 
 # Run Python Script 
 python3 "$PYTHON_SCRIPT"
